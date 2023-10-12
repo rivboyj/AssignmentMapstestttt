834ae614eb1abfd40986254cd39ce12e962bc3c2
rivboyj <rijo7618@colorado.edu>
2023-10-11 20:03:45 -0600

    Initial Commit

---
+++ Assignment6/Assignment6App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment6App.swift
+//  Assignment6
+//
+//  Created by River on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment6
+//
+//  Created by River on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
